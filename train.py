e25065f9d425b51896bcb8831a4e19265adce5e8
Oluwaseun Ilori <jideilori77@gmail.com>
2022-12-20 21:15:05 +0100

    modified train

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 # Compile & Train
-import tensorflow
+import tensorflow as tf
 from loss import depth_loss_function
 import os
 from load_data import DataLoader
@@ -8,7 +8,7 @@ from model import MobileNetv3_model
 # Parameters
 batch_size     = 16
 learning_rate  = 0.0001
-epochs         = 10
+epochs         = 1
 
 
 dl = DataLoader()
@@ -18,13 +18,25 @@ print('Data loader ready.')
 
 
 
-optimizer = tensorflow.keras.optimizers.Adam(learning_rate=learning_rate, amsgrad=True)
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, amsgrad=True)
 
 MobileNetv3_model.compile(loss=depth_loss_function, optimizer=optimizer)
 
 # Create checkpoint callback
 checkpoint_path = "training_1/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
-cp_callback = tensorflow.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, verbose=1)
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, verbose=1)
 
 MobileNetv3_model.fit(train_generator, epochs=5, steps_per_epoch=dl.length//batch_size, callbacks=[cp_callback])
+
+
+MobileNetv3_model.save('model_full.h5')
+MobileNetv3_model = tf.keras.models.load_model('model_full.h5', compile=False)
+
+# Converting a tf.Keras model to a TensorFlow Lite model.
+converter = tf.lite.TFLiteConverter.from_keras_model(MobileNetv3_model)
+tflite_model = converter.convert()
+
+# Save the TF Lite model.
+with tf.io.gfile.GFile('mobilenetv3_model.tflite', 'wb') as f:
+  f.write(tflite_model)
